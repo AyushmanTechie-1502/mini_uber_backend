5e89fdab6e9539f23b925b45917d7149fa375a0f
Ayushman <ayushman1502singh@gmail.com>
2026-06-01 02:01:03 +0530

    Intital setup done: Fixedport collision with 3307, added docker-compose and controller file

---
+++ src/main/java/com/uber/backend/UberNetworkController.java
@@ -0,0 +1,9 @@
+package com.uber.backend;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+@RestController
+@RequestMapping
+public class UberNetworkController {
+}
